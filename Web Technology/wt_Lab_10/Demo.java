1c860c0f48c46930bf528ca312d57e7c9fdbcedb
Divyanshu975 <112746483+Divyanshu975@users.noreply.github.com>
2023-04-03 22:55:42 +0530

    Create Demo.java

---
+++ Web Technology/wt_Lab_10/Demo.java
@@ -0,0 +1,10 @@
+import Geometry.*;
+
+public class Demo {
+    public static void main(String[] args) {
+        circle c1=new circle();
+        c1.setradius(8.5);
+        double r=c1.getRadius();
+        System.out.println("The radius of the circle is "+r);
+    }
+}
