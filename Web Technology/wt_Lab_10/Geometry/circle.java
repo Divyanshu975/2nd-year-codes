11a9e6d887f5e869d4abb3cda384b91647b00690
Divyanshu975 <112746483+Divyanshu975@users.noreply.github.com>
2023-04-03 22:56:33 +0530

    Interface and package

---
+++ Web Technology/wt_Lab_10/Geometry/circle.java
@@ -0,0 +1,11 @@
+package Geometry;
+
+public class circle {
+    double radius;
+    public void setradius(double r){
+        this.radius=r;
+    }
+    public double getRadius(){
+        return this.radius;
+    }
+}
